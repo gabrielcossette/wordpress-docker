f0549d23cd41335ca7ffbacb19311e2e4baa29b7
Gabriel Cossette <gabriel@gcossette.com>
2018-04-29 14:40:30 -0400

    Update

--- create_patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/docker-library/wordpress/master/php7.2/fpm-alpine/docker-entrypoint.sh -O docker-entrypoint.sh
-
-diff -Naur docker-entrypoint.sh entrypoint_modif > entrypoint.patch
